e8c95ffb0c8c1e775aab43c9b7d0ebe1a744f9cd
root <root@Surfacebook-Dailey.localdomain>
2019-08-14 20:53:03 -0700

    Updates

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Portfolio from '../components/Portfolio';
-
-export default function App() {
-  return (
-    <Portfolio/>
-  );
-}
---
+++ styles-a61958965bf0b7f253b6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{187:function(n,w,o){}}]);
+//# sourceMappingURL=styles-a61958965bf0b7f253b6.js.map
