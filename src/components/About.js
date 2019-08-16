b2c323a233e2308bfeaf4ba1da2df15ca4b3a18c
rdailey98 <ryan.dailey@hbo.com>
2019-08-16 08:33:04 -0700

    cleanup

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,21 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Typography from '@material-ui/core/Typography';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import Card from '@material-ui/core/Card';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import CardMedia from '@material-ui/core/CardMedia';
-import Surfing from '../assets/surfing.jpeg';
-import Race from '../assets/race.jpeg';
-import Skiing from '../assets/skiing.jpeg';
-import 'typeface-roboto';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+
+import "typeface-roboto";
 
 function Footer() {
   return (
-    <Typography 
-      variant="body2" 
-      color="textSecondary" 
-      align="center"
-    >
+    <Typography variant="body2" color="textSecondary" align="center">
       {"© 2019, Ryan Dailey "}
     </Typography>
   );
@@ -31,40 +23,39 @@ function Footer() {
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: '100%',
+    width: "100%"
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
-    fontWeight: theme.typography.fontWeightRegular,
+    fontWeight: theme.typography.fontWeightRegular
   },
   leftIcon: {
-    marginRight: theme.spacing(1),
+    marginRight: theme.spacing(1)
   },
   cardGrid: {
-    paddingTop: theme.spacing(8),
+    paddingTop: theme.spacing(10),
     paddingBottom: theme.spacing(8),
     paddingLeft: theme.spacing(16),
     paddingRight: theme.spacing(16),
-    backgroundColor: theme.palette.background.secondary, 
+    backgroundColor: theme.palette.background.secondary
   },
   card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    height: "100%",
+    display: "flex",
+    flexDirection: "column"
   },
   cardMedia: {
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%" // 16:9
   },
   footer: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
+    padding: theme.spacing(6)
   },
   photoSize: {
     height: 200,
-    width: 200,
-  },
+    width: 200
+  }
 }));
-
 
 export default function SimpleExpansionPanel() {
   const classes = useStyles();
@@ -73,133 +64,99 @@ export default function SimpleExpansionPanel() {
     <div className={classes.root}>
       <AppBar position="relative">
         <Toolbar>
-          <Button
-            href="/"
-            variant="contained" 
-            color="primary"
-          >
+          <Button href="/" variant="contained" color="primary">
             <ArrowBackIcon className={classes.leftIcon} />
             Back
           </Button>
         </Toolbar>
       </AppBar>
-      <Container 
-        className={classes.cardGrid} 
-        maxWidth="false"
-      >
-        {/* End hero unit */}
+      <Container className={classes.cardGrid} maxWidth="false">
         <Grid container spacing={4}>
-            <Grid item 
-              xs={12} 
-              sm={6} 
-              md={4}
-            >
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image={Race}
-                />
-              </Card>
-            </Grid>
-            <Grid item 
-              xs={12} 
-              sm={6} 
-              md={4}
-            >
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image={Skiing}
-                />
-              </Card>
-            </Grid>
-            <Grid item 
-              xs={12} 
-              sm={6} 
-              md={4}
-            >
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image={Surfing}
-                />
-              </Card>
-            </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography variant="h6" color="primary">
+                  About Me
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Typography>
+                  I'm currently a senior at the University of Washington
+                  studying Computer Science. I grew up in Spokane, Washington,{" "}
+                  <br /> and like to spend time outdoors when I'm not stuck on
+                  campus with a book in my nose. At home, I enjoy cooking for{" "}
+                  <br /> myself and reading or watching Netflix. I love to
+                  travel and work in new places, but will find myself coming 
+                  back to the pacific <br /> northwest my home.
+                </Typography>
+              </ExpansionPanelDetails>
+              <ExpansionPanelDetails>
+                <Typography>
+                  After graduation, I hope to see myself in a role that is
+                  consumer facing. With two summers of experience as an intern{" "}
+                  <br /> at HBO, I've realized I get great satisfaction from
+                  working on products that directly interface with users. More
+                  so, I've <br /> grown fond of the logical and critical
+                  thinking which comes with backend development. Video
+                  streaming, UI, and user <br /> account based work are areas
+                  that I have substantial experience in. In my free time, I
+                  enjoy web development and the <br /> creative side that comes
+                  with designing and using frameworks like React.
+                </Typography>
+              </ExpansionPanelDetails>
+              <ExpansionPanelDetails>
+                <Typography>
+                  Please see my LinkedIn if you'd like to know more.
+                </Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography variant="h6" color="primary">
+                  Contact Info
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Typography>dailey.ryan@outlook.com</Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                aria-controls="panel3a-content"
+                id="panel3a-header"
+              >
+                <Typography variant="h6" color="primary">
+                  Technical Proficiencies
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Typography>
+                  Programming languages & frameworks: Java, Javascript, HTML, C,
+                  C++, SQL, React, Node.js <br />
+                  Databases: PostgreSQL, MySQL <br />
+                  Servers: Azure, AWS, Apache <br />
+                  Tools: Git, JIRA, Jenkins, Docker, JUnit, Kubernetes, Jasmine{" "}
+                  <br />
+                  Methodologies: SAFe (Scaled Agile Framework), Scrum <br />
+                </Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </Grid>
         </Grid>
       </Container>
-      <ExpansionPanel>
-        <ExpansionPanelSummary
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography 
-            variant="h6" 
-            color="primary"
-          >
-            About Me
-          </Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            I'm currently a senior at the University of Washington studying Computer Science.
-            I grew up in Spokane, Washington, <br/> and like to spend time outdoors when I'm not stuck on campus with a book in my nose. At home, I enjoy cooking for <br/> myself and reading or watching Netflix. I'm open to travelling and working in new places, but will always call the pacific <br/> northwest my home.
-          </Typography>
-        </ExpansionPanelDetails>
-        <ExpansionPanelDetails>
-          <Typography>
-            After graduation, I hope to see myself in a role that is consumer facing. With two summers of experience as an intern <br/> at HBO, I've realized I get great satisfaction from working on products that directly interface with users. More so, I've <br/> grown fond of the logical and critical thinking which comes with backend development. Video streaming, UI, and user <br/> account based work are areas that I have substantial experience in. In my free time, I enjoy web development and the <br/> creative side that comes with designing and using frameworks like React.
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-      <ExpansionPanel>
-        <ExpansionPanelSummary
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography 
-            variant="h6" 
-            color="primary"
-          >
-            Contact Info
-          </Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            dailey.ryan@outlook.com
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-      <ExpansionPanel>
-        <ExpansionPanelSummary
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-          <Typography 
-            variant="h6" 
-            color="primary"
-          >
-            Technical Proficiencies
-          </Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            Programming languages & frameworks: Java, Javascript, HTML, C, C++, SQL, React, Node.js <br/>
-            Databases: PostgreSQL, MySQL <br/>
-            Servers: Azure, AWS, Apache <br/>
-            Tools: Git, JIRA, Jenkins, Docker, JUnit, Kubernetes, Jasmine <br/>
-            Methodologies: SAFe (Scaled Agile Framework), Scrum <br/>
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
       <footer className={classes.footer}>
         <Footer />
       </footer>
     </div>
   );
 }
-
-
-
-
-
-
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import CodeIcon from '@material-ui/icons/Code';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Profile from '../assets/profile.png';
-import Flight from '../assets/airplane.png';
-import Xv6 from '../assets/os.png';
-import 'typeface-roboto';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import CodeIcon from "@material-ui/icons/Code";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Profile from "../assets/profile.png";
+import Flight from "../assets/airplane.png";
+import Xv6 from "../assets/os.png";
+import "typeface-roboto";
 
 function Footer() {
   return (
-    <Typography 
-      variant="body2" 
-      color="textSecondary" 
-      align="center"
-    >
+    <Typography variant="body2" color="textSecondary" align="center">
       {"© 2019, Ryan Dailey "}
     </Typography>
   );
@@ -31,37 +27,37 @@ function Footer() {
 
 const useStyles = makeStyles(theme => ({
   icon: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   heroContent: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
+    padding: theme.spacing(8, 0, 6)
   },
   heroButtons: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(4)
   },
   cardGrid: {
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
     paddingLeft: theme.spacing(8),
-    paddingRight: theme.spacing(8),    
+    paddingRight: theme.spacing(8),
     backgroundColor: theme.palette.background.secondary
   },
   card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    height: "100%",
+    display: "flex",
+    flexDirection: "column"
   },
   cardMedia: {
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%" // 16:9
   },
   cardContent: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   footer: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
+    padding: theme.spacing(6)
+  }
 }));
 
 export default function Album() {
@@ -73,11 +69,7 @@ export default function Album() {
       <AppBar position="relative">
         <Toolbar>
           <CodeIcon className={classes.icon} />
-          <Typography 
-            variant="h6" 
-            color="inherit" 
-            noWrap
-          >
+          <Typography variant="h6" color="inherit" noWrap>
             Ryan Dailey
           </Typography>
         </Toolbar>
@@ -85,45 +77,38 @@ export default function Album() {
       <main>
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
-            <Typography 
-              component="h1" 
-              variant="h2" 
-              align="center" 
-              color="textPrimary" 
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
               gutterBottom
             >
               Projects
             </Typography>
             <div className={classes.heroButtons}>
-              <Grid 
-                container spacing={2} 
-                justify="center" 
-                alignItems="center"
-              >
+              <Grid container spacing={2} justify="center" alignItems="center">
                 <Grid item>
-                  <Button
-                    href="/about"
-                    variant="contained" 
-                    color="primary"
-                  >
+                  <Button href="/about" variant="contained" color="primary">
                     About me
                   </Button>
                 </Grid>
                 <Grid item>
-                  <Button 
-                    target="_blank" 
+                  <Button
+                    target="_blank"
                     href="https://www.linkedin.com/in/dailey-ryan/"
-                    variant="outlined" 
+                    variant="outlined"
                     color="secondary"
                   >
                     LinkedIn
                   </Button>
                 </Grid>
                 <Grid item>
-                  <Button 
+                  <Button
                     target="_blank"
-                    href="https://github.com/rdailey98" 
-                    variant="outlined" color="secondary"
+                    href="https://github.com/rdailey98"
+                    variant="outlined"
+                    color="secondary"
                   >
                     Github
                   </Button>
@@ -132,16 +117,9 @@ export default function Album() {
             </div>
           </Container>
         </div>
-        <Container 
-          className={classes.cardGrid} 
-          maxWidth="false"
-        >
+        <Container className={classes.cardGrid} maxWidth="false">
           <Grid container spacing={4}>
-            <Grid item 
-              xs={12} 
-              sm={6} 
-              md={4}
-            >
+            <Grid item xs={12} sm={6} md={4}>
               <Card className={classes.card}>
                 <CardMedia
                   className={classes.cardMedia}
@@ -149,35 +127,32 @@ export default function Album() {
                   title="Image title"
                 />
                 <CardContent className={classes.cardContent}>
-                  <Typography 
-                    gutterBottom 
-                    variant="h5" 
-                    component="h2"
-                  >
+                  <Typography gutterBottom variant="h5" component="h2">
                     This Website
                   </Typography>
-                  <Typography variant="overline">
-                    Overview:
-                  </Typography>
+                  <Typography variant="overline">Overview:</Typography>
                   <Typography>
-                    This website is a React application built with the Gatsby.js framework
-                    to house some of the projects I've completed. It contains an about me section and links to both my LinkedIn and Github accounts. The UI was designed with the Material-UI framework.
+                    This website is a React application built with the Gatsby.js
+                    framework to house some of the projects I've completed. It
+                    contains an about me section and links to both my LinkedIn
+                    and Github accounts. The UI was designed with the
+                    Material-UI framework.
                   </Typography>
-                  <Typography variant="overline">
-                    Experience Gained:
-                  </Typography>
+                  <Typography variant="overline">Experience Gained:</Typography>
                   <Typography>
-                    - React <br/>
-                    - Material-UI <br/>
-                    - HTML <br/>
-                    - Networking and site hosting <br/>
-                    - UI Design <br/>
-                    - NPM (Node package manager) <br/>
+                    - React <br />
+                    - Material-UI <br />
+                    - HTML <br />
+                    - Networking and site hosting <br />
+                    - UI Design <br />
+                    - NPM (Node package manager) <br />
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button 
-                    size="large" 
+                  <Button
+                    target="_blank"
+                    href="https://github.com/rdailey98/portfolio/tree/public"
+                    size="large"
                     color="primary"
                   >
                     View
@@ -185,11 +160,7 @@ export default function Album() {
                 </CardActions>
               </Card>
             </Grid>
-            <Grid item 
-              xs={12} 
-              sm={6} 
-              md={4}
-            >
+            <Grid item xs={12} sm={6} md={4}>
               <Card className={classes.card}>
                 <CardMedia
                   className={classes.cardMedia}
@@ -197,38 +168,35 @@ export default function Album() {
                   title="Image title"
                 />
                 <CardContent className={classes.cardContent}>
-                  <Typography 
-                    gutterBottom 
-                    variant="h5" 
-                    component="h2"
-                  >
+                  <Typography gutterBottom variant="h5" component="h2">
                     Flight Booker
                   </Typography>
-                  <Typography variant="overline">
-                    Overview:
-                  </Typography>
+                  <Typography variant="overline">Overview:</Typography>
                   <Typography gutterBottom>
-                    A simple flight booking system built with Java and housed on a Microsoft Azure database. The application is meant to be a play tool. It supports user account creation, fund deposit and withdrawl, itinerary searching, and flight booking/cancellation. The application is fully supportive of concurrent transactions, and can be run on a localhost.
+                    A simple flight booking system built with Java and housed on
+                    a Microsoft Azure database. The application is meant to be a
+                    play tool. It supports user account creation, fund deposit
+                    and withdrawl, itinerary searching, and flight
+                    booking/cancellation. The application is fully supportive of
+                    concurrent transactions, and can be run on a localhost.
                   </Typography>
-                  <Typography variant="overline">
-                    Experience Gained:
-                  </Typography>
+                  <Typography variant="overline">Experience Gained:</Typography>
                   <Typography>
-                    - Database deployment <br/>
-                    - Microsoft Azure <br/>
-                    - SQL <br/>
-                    - Distrubted system design <br/>
-                    - Java <br/>
-                    - Schema design <br/>
-                    - Transactions <br/>
-                    - Unit and integration testing <br/>            
+                    - Database deployment <br />
+                    - Microsoft Azure <br />
+                    - SQL <br />
+                    - Distrubted system design <br />
+                    - Java <br />
+                    - Schema design <br />
+                    - Transactions <br />
+                    - Unit and integration testing <br />
                   </Typography>
                 </CardContent>
                 <CardActions>
                   <Button
-                    target="_blank" 
-                    href="https://github.com/rdailey98/flight-booker" 
-                    size="large" 
+                    target="_blank"
+                    href="https://github.com/rdailey98/flight-booker"
+                    size="large"
                     color="primary"
                   >
                     View
@@ -236,11 +204,7 @@ export default function Album() {
                 </CardActions>
               </Card>
             </Grid>
-            <Grid item 
-              xs={12} 
-              sm={6} 
-              md={4}
-            >
+            <Grid item xs={12} sm={6} md={4}>
               <Card className={classes.card}>
                 <CardMedia
                   className={classes.cardMedia}
@@ -248,39 +212,37 @@ export default function Album() {
                   title="Image title"
                 />
                 <CardContent className={classes.cardContent}>
-                  <Typography 
-                    gutterBottom 
-                    variant="h5" 
-                    component="h2"
-                  >
+                  <Typography gutterBottom variant="h5" component="h2">
                     Xv6 Operating System
                   </Typography>
-                  <Typography variant="overline">
-                    Overview:
-                  </Typography>
+                  <Typography variant="overline">Overview:</Typography>
                   <Typography>
-                    Xv6 is an operating system developed by MIT that is stubbed out and meant to be used as a learning tool. It is largely skeleton code and has been left for the 
-                    developer implement. After completing the project, I took Xv6 from a
-                    non-functioning OS to one that supports a file system, system calls such as
-                    exec, fork, read, write, and open, multi-processing capabilities, disk 
-                    reads and writes, paging, a crash safety boot mechanism, and per process priority tables.
+                    Xv6 is an operating system developed by MIT that is stubbed
+                    out and meant to be used as a learning tool. It is largely
+                    skeleton code and has been left for the developer implement.
+                    After completing the project, I took Xv6 from a
+                    non-functioning OS to one that supports a file system,
+                    system calls such as exec, fork, read, write, and open,
+                    multi-processing capabilities, disk reads and writes,
+                    paging, a crash safety boot mechanism, and per process
+                    priority tables.
                   </Typography>
-                  <Typography variant="overline">
-                    Experience Gained:
-                  </Typography>
+                  <Typography variant="overline">Experience Gained:</Typography>
                   <Typography>
-                    - Debugging techniques <br/>
-                    - C <br/>
-                    - System design <br/>
-                    - Unit and integration testing <br/>
-                    - Concurrency <br/>
-                    - Deadlock and race condition handling <br/>
-                    - Multi-processing development <br/>  
+                    - Debugging techniques <br />
+                    - C <br />
+                    - System design <br />
+                    - Unit and integration testing <br />
+                    - Concurrency <br />
+                    - Deadlock and race condition handling <br />
+                    - Multi-processing development <br />
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button 
-                    size="large" 
+                  <Button
+                    target="_blank"
+                    href="https://github.com/rdailey98/xv6-operating-system"
+                    size="large"
                     color="primary"
                   >
                     View
